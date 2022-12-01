d3fa2b686b450c3263cb3c61b9f591a91ae615d1
Batutist <Batutist@gmail.com>
2022-12-01 12:40:52 -0800

    SeatsView file created

---
+++ MovieBooking/Views/SeatsView.swift
@@ -0,0 +1,20 @@
+//
+//  SeatsView.swift
+//  MovieBooking
+//
+//  Created by Sergey Kovalev on 12/1/22.
+//
+
+import SwiftUI
+
+struct SeatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SeatsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SeatsView()
+    }
+}
