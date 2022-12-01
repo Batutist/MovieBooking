c012f631d34c091eae1fb59f5ce1887850c69760
Batutist <Batutist@gmail.com>
2022-12-01 11:21:50 -0800

    Booking View file created. Start working on the layout for the BookingView

---
+++ MovieBooking/Views/BookingView.swift
@@ -0,0 +1,126 @@
+//
+//  BookingView.swift
+//  MovieBooking
+//
+//  Created by Sergey Kovalev on 12/1/22.
+//
+
+import SwiftUI
+
+struct BookingView: View {
+    @State var gradient = [Color("backgroundColor2").opacity(0), Color("backgroundColor2"), Color("backgroundColor2"), Color("backgroundColor2")]
+    
+    @State var mockSelected = false
+    @State var selectedDate = false
+    @State var datePressed = false
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Image("booking")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxHeight: .infinity, alignment: .top)
+                
+                // Gradient for the image fade effect
+                VStack {
+                    LinearGradient(colors: gradient, startPoint: .top, endPoint: .bottom)
+                        .frame(height: geometry.size.height / 1.5)
+                }
+                .frame(maxHeight: .infinity, alignment: .bottom)
+                
+                // Custom back navigation and options buttons
+                VStack(spacing: 0) {
+                    HStack {
+                        // Back button
+                        CircleButton {
+                            
+                        }
+                        
+                        Spacer()
+                        
+                        // Options button
+                        CircleButton(action: {
+                            
+                        }, image: "ellipsis")
+                    }
+                    .padding(EdgeInsets(top: 46, leading: 20, bottom: 0, trailing: 20))
+                    
+
+                    
+                }
+                .frame(maxHeight: .infinity, alignment: .top)
+              
+                // Main Content
+                VStack {
+                    labels
+                    
+                    dateCards
+                }
+            }
+            .background(Color("backgroundColor2"))
+            .ignoresSafeArea()
+        }
+    }
+    
+    private var content: some View {
+        VStack {
+            labels
+            
+            dateCards
+        }
+    }
+    
+    
+    // Movie labels
+    private var labels: some View {
+        VStack(spacing: 30) {
+            VStack(spacing: 0) {
+                Text("Doctor Strange")
+                    .font(.title3.bold())
+                
+                
+                Text("in the Multiverse of Madness")
+                    .font(.headline).opacity(0.9)
+            }
+            Text("Dr. Stephen Strange casts a forbidden spell that opens the doorway to the multiverse, including alternate versions of... read more")
+                .font(.subheadline)
+                .padding(.horizontal, 30)
+            
+            Text("Select date and time")
+                .font(.headline)
+        }
+        .foregroundColor(.white)
+    }
+    
+    // Date Cards
+    private var dateCards: some View {
+        HStack(alignment: .top, spacing: 20) {
+            DateButton(weekDay: "Thu", numDay: "1", isSelected: $mockSelected) {}
+                .padding(.top, 90)
+            
+            DateButton(weekDay: "Fri", numDay: "2", isSelected: $mockSelected) {}
+                .padding(.top, 70)
+            
+            DateButton(weekDay: "Sat", numDay: "3", width: 70, height: 100, isSelected: $selectedDate) {
+                
+                withAnimation(.spring()) {
+                    selectedDate.toggle()
+                }
+            }
+            .padding(.top, 30)
+            
+            DateButton(weekDay: "Sun", numDay: "4", isSelected: $mockSelected) {}
+                .padding(.top, 70)
+            
+            DateButton(weekDay: "Mon", numDay: "5", isSelected: $mockSelected) {}
+                .padding(.top, 90)
+        }
+    }
+}
+
+struct BookingView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookingView()
+    }
+}
